db5b1ba93e009046905877ce33d248561e36dc75
Vadym <v.lytvynets61@gmail.com>
2020-03-26 22:56:13 +0200

    Refactoring

--- training.kt
+++ training.kt
@@ -1,132 +1,137 @@
 import org.knowm.xchart.SwingWrapper
+import org.knowm.xchart.XYChart
 import org.knowm.xchart.XYChartBuilder
 import org.knowm.xchart.XYSeries
 import org.knowm.xchart.style.Styler
 import org.knowm.xchart.style.markers.SeriesMarkers
 import java.io.FileInputStream
+import java.io.InputStream
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.LinkedHashMap
 import kotlin.math.abs
-import kotlin.math.min
-
-val cars = ArrayList<Car>()
-val normalizedCars = ArrayList<NormalizedCar>()
-
-var bias: Double = 800.0
-var slope: Double = -900.0
+import kotlin.system.exitProcess
 
 const val LEARNING_RATE = 0.1
+const val ACCEPTABLE_LEARNING_STEP_DIFF = 0.01
 
-const val ACCEPTED_STEP_DIFF = 0.01
+data class Car(val mileage: Double, val price: Int)
+
+val inputCars = ArrayList<Car>()
+val reducedMileageCars = ArrayList<Car>()
+var averageMileage: Double = 0.0
+
+var bias: Double = 0.0
+var slope: Double = 0.0
 
 fun main() {
-	readInput()
-
-	normalize()
-
-	findWeights()
-
-//	drawChart()
+	readInput(FileInputStream("src/data.csv"))
+	averageMileage = inputCars.sumByDouble { it.mileage } / inputCars.size
+	reduceMileage()
+	calculateWeights()
+	drawChart()
 }
 
-var maxMileage: Int = 0
-var minMileage: Int = 0
-var maxPrice: Int = 0
-var minPrice: Int = 0
-var averageMileage: Int = 0
-
-fun normalize() {
-	maxMileage = cars.maxBy { it.mileage }!!.mileage
-	minMileage = cars.minBy { it.mileage }!!.mileage
-	maxPrice = cars.maxBy { it.price }!!.price
-	minPrice = cars.minBy { it.price }!!.price
-
-	averageMileage = cars.sumBy { it.mileage } / cars.size
-
-	cars.forEach {
-		val normalizedMileage = it.mileage / 300_000.0
-//		val normalizedPrice = (it.price - minPrice).toDouble() / (maxPrice - minPrice).toDouble()
-		normalizedCars.add(NormalizedCar(normalizedMileage, it.price.toDouble()))
+fun readInput(inputStream: InputStream) {
+	val scanner = Scanner(inputStream)
+	val csvTitle = scanner.nextLine()
+	val values = csvTitle.split(',')
+	if (values[0] != "km" || values[1] != "price") {
+		invalidExit("Wrong csv format")
+	}
+	try {
+		while (scanner.hasNextLine()) {
+			val values = scanner.nextLine().split(',')
+			inputCars.add(Car(values[0].toDouble(), values[1].toInt()))
+		}
+	} catch (e: Exception) {
+		invalidExit("Wrong csv format")
 	}
 }
 
-fun estimatePrice(mileage: Double) =
-	mileage * slope + bias
+fun reduceMileage() {
+	inputCars.forEach {
+		val reducedMileage = it.mileage / averageMileage
+		reducedMileageCars.add(Car(reducedMileage, it.price))
+	}
+}
 
-fun findWeights() {
+fun calculateWeights() {
 	while(true) {
-		println(String.format("tuned slope: %2.2f", getTunedSlope()))
-		println(String.format("tuned bias: %2.2f", getTunedBias()))
-		val newSlope = getTunedSlope()
-		val newBias = getTunedBias()
-		if (abs(newSlope - slope) <= ACCEPTED_STEP_DIFF && abs(newBias - bias) <= ACCEPTED_STEP_DIFF) {
-			bias = newBias
-			slope = newSlope
+		val newSlope = getSlopeAfterStep()
+		val newBias = getBiasAfterStep()
+		if (isConvergenceAchieved(newSlope, newBias)) {
 			break
 		}
 		bias = newBias
 		slope = newSlope
 	}
-	println("average: ${averageMileage}")
+	slope /= averageMileage
 	println("estimated price")
-	slope /= 300_000.0
-	println(estimatePrice(240000.0))
+	println(estimatePrice(74000.0))
 	println("slope: $slope")
 	println("bias: $bias")
 }
 
-fun getTunedSlope(): Double =
-	slope - (normalizedCars.sumByDouble { (estimatePrice(it.mileage) - it.price) * it.mileage } / normalizedCars.size) * LEARNING_RATE
+private fun isConvergenceAchieved(newSlope: Double, newBias: Double) =
+	abs(newSlope - slope) <= ACCEPTABLE_LEARNING_STEP_DIFF && abs(newBias - bias) <= ACCEPTABLE_LEARNING_STEP_DIFF
 
-fun getTunedBias(): Double =
-	bias - (normalizedCars.sumByDouble { estimatePrice(it.mileage) - it.price } / normalizedCars.size) * LEARNING_RATE
+fun getSlopeAfterStep(): Double =
+	slope - (reducedMileageCars.sumByDouble { (estimatePrice(it.mileage) - it.price) * it.mileage } / reducedMileageCars.size) * LEARNING_RATE
 
-private fun drawChart() {
-	val startX = cars.minBy { it.mileage }!!.mileage
-	val endX = cars.maxBy { it.mileage }!!.mileage
-	val startY = estimatePrice((startX - minMileage).toDouble() / (maxMileage - minMileage).toDouble())
-	val endY = estimatePrice((endX - minMileage).toDouble() / (maxMileage - minMileage).toDouble())
-	val xLineData = doubleArrayOf(startX.toDouble(), endX.toDouble())
-	val yLineData = doubleArrayOf(startY, endY)
+fun getBiasAfterStep(): Double =
+	bias - (reducedMileageCars.sumByDouble { estimatePrice(it.mileage) - it.price } / reducedMileageCars.size) * LEARNING_RATE
 
-	val chart = XYChartBuilder()
-			.width(600)
-			.height(500)
-			.title("Training Result")
-			.xAxisTitle("mileage")
-			.yAxisTitle("price")
-			.build()
+fun estimatePrice(mileage: Double) =
+	mileage * slope + bias
 
-	with(chart.styler) {
-		defaultSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Scatter
-//		isChartTitleVisible = false
-		legendPosition = Styler.LegendPosition.InsideSW
-		markerSize = 8
-		xAxisDecimalPattern = ""
-	}
-
-	val carsX = cars.map { it.mileage }
-	val carsY = cars.map { it.price }
-
-	chart.addSeries("Car", carsX, carsY)
-
-	val series = chart.addSeries("Linear regression", xLineData, yLineData)
-	series.marker = SeriesMarkers.NONE
-	series.xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
-
-
+fun drawChart() {
+	val chart = createChart()
+	addCarMarkersOnChart(chart)
+	addLinearRegLineOnChart(chart)
 	SwingWrapper(chart).displayChart()
 }
 
-fun readInput() {
-	val scanner = Scanner(FileInputStream("src/data.csv"))
-	scanner.nextLine()
-	while (scanner.hasNextLine()) {
-		val values = scanner.nextLine().split(',')
-		cars.add(Car(values[0].toInt(), values[1].toInt()))
+fun createChart(): XYChart {
+	val chart = XYChartBuilder()
+		.width(600)
+		.height(500)
+		.title("Training Result")
+		.xAxisTitle("mileage")
+		.yAxisTitle("price")
+		.build()
+
+	with(chart.styler) {
+		defaultSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Scatter
+		legendPosition = Styler.LegendPosition.InsideSW
+		markerSize = 8
+		xAxisDecimalPattern = ""
+		yAxisDecimalPattern = ""
 	}
+
+	return chart
 }
 
-data class Car(val mileage: Int, val price: Int)
-data class NormalizedCar(val mileage: Double, val price: Double)
+fun addCarMarkersOnChart(chart: XYChart) {
+	val carsX = inputCars.map { it.mileage }
+	val carsY = inputCars.map { it.price }
+
+	chart.addSeries("Car", carsX, carsY)
+}
+
+fun addLinearRegLineOnChart(chart: XYChart) {
+	val xLinearRegStart = inputCars.minBy { it.mileage }!!.mileage
+	val xLinearRegEnd = inputCars.maxBy { it.mileage }!!.mileage
+	val yLinearRegStart = estimatePrice(xLinearRegStart)
+	val yLinearRegEnd = estimatePrice(xLinearRegEnd)
+	val xLinearRegData = doubleArrayOf(xLinearRegStart, xLinearRegEnd)
+	val yLinearRegData = doubleArrayOf(yLinearRegStart, yLinearRegEnd)
+
+	val series = chart.addSeries("Linear regression", xLinearRegData, yLinearRegData)
+	series.marker = SeriesMarkers.NONE
+	series.xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+}
+
+fun invalidExit(errorMsg: String) {
+	println(errorMsg)
+	exitProcess(1)
+}
