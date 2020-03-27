e71d00b971bc0c7ce6943749f72df6ef4b31f88d
Vadym <v.lytvynets61@gmail.com>
2020-03-27 10:21:55 +0200

    Refactoring and command line arguments in training

--- training.kt
+++ training.kt
@@ -1,3 +1,4 @@
+import com.xenomachina.argparser.*
 import org.knowm.xchart.SwingWrapper
 import org.knowm.xchart.XYChart
 import org.knowm.xchart.XYChartBuilder
@@ -5,14 +6,15 @@ import org.knowm.xchart.XYSeries
 import org.knowm.xchart.style.Styler
 import org.knowm.xchart.style.markers.SeriesMarkers
 import java.io.FileInputStream
+import java.io.IOException
 import java.io.InputStream
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.system.exitProcess
 
-const val LEARNING_RATE = 0.1
-const val ACCEPTABLE_LEARNING_STEP_DIFF = 0.01
+const val DEFAULT_LEARNING_RATE = 0.1
+const val DEFAULT_ACCEPTABLE_LEARNING_STEP_DIFF = 0.01
 
 data class Car(val mileage: Double, val price: Int)
 
@@ -20,24 +22,48 @@ val inputCars = ArrayList<Car>()
 val reducedMileageCars = ArrayList<Car>()
 var averageMileage: Double = 0.0
 
+var learningRate = DEFAULT_LEARNING_RATE
+var acceptableLearningStepDiff = DEFAULT_ACCEPTABLE_LEARNING_STEP_DIFF
+
 var bias: Double = 0.0
 var slope: Double = 0.0
 
-fun main() {
-	readInput(FileInputStream("src/data.csv"))
+//https://medium.com/@lachlanmiller_52885/machine-learning-week-1-cost-function-gradient-descent-and-univariate-linear-regression-8f5fe69815fd
+fun main(args: Array<String>) {
+	val clArgs = parseArgs(args)
+	learningRate = clArgs.learningRage
+	acceptableLearningStepDiff = clArgs.acceptableLearningStepDiff
+	processInput(clArgs.inputFileName)
 	averageMileage = inputCars.sumByDouble { it.mileage } / inputCars.size
 	reduceMileage()
-	calculateWeights()
-	drawChart()
+	calculateWeights(clArgs.verbose)
+	printResult()
+	if (clArgs.showChart) { drawChart() }
+}
+
+fun parseArgs(args: Array<String>): CommandLineArguments {
+	return try {
+		ArgParser(args).parseInto(::CommandLineArguments)
+	} catch (e: SystemExitException) {
+		e.printAndExit()
+	}
+}
+
+fun processInput(inputFileName: String?) {
+	if (inputFileName == null) {
+		readInput(System.`in`)
+	} else {
+		try {
+			FileInputStream(inputFileName).use(::readInput)
+		} catch (e: IOException) {
+			invalidExit("Can't open file or file doesn't exist")
+		}
+	}
 }
 
 fun readInput(inputStream: InputStream) {
 	val scanner = Scanner(inputStream)
-	val csvTitle = scanner.nextLine()
-	val values = csvTitle.split(',')
-	if (values[0] != "km" || values[1] != "price") {
-		invalidExit("Wrong csv format")
-	}
+	processCSVHeader(scanner)
 	try {
 		while (scanner.hasNextLine()) {
 			val values = scanner.nextLine().split(',')
@@ -48,6 +74,18 @@ fun readInput(inputStream: InputStream) {
 	}
 }
 
+private fun processCSVHeader(scanner: Scanner) {
+	if (scanner.hasNextLine()) {
+		val csvHeader = scanner.nextLine()
+		val csvTitles = csvHeader.split(',')
+		if (csvTitles[0] != "km" || csvTitles[1] != "price") {
+			invalidExit("Wrong csv format")
+		}
+	} else {
+		invalidExit("Input is empty")
+	}
+}
+
 fun reduceMileage() {
 	inputCars.forEach {
 		val reducedMileage = it.mileage / averageMileage
@@ -55,8 +93,12 @@ fun reduceMileage() {
 	}
 }
 
-fun calculateWeights() {
+fun calculateWeights(printProgress: Boolean) {
+	var iteration = 0
 	while(true) {
+		if (printProgress) {
+			printIterationResult(iteration)
+		}
 		val newSlope = getSlopeAfterStep()
 		val newBias = getBiasAfterStep()
 		if (isConvergenceAchieved(newSlope, newBias)) {
@@ -64,25 +106,34 @@ fun calculateWeights() {
 		}
 		bias = newBias
 		slope = newSlope
+		iteration++
 	}
 	slope /= averageMileage
-	println("estimated price")
-	println(estimatePrice(74000.0))
-	println("slope: $slope")
-	println("bias: $bias")
+}
+
+private fun printIterationResult(iteration: Int) {
+	println("ITERATION: $iteration")
+	println("Theta0 (bias): " + String.format("%.6f", bias))
+	println("Theta1 (slope): " + String.format("%.6f", slope / averageMileage))
+	println()
 }
 
 private fun isConvergenceAchieved(newSlope: Double, newBias: Double) =
-	abs(newSlope - slope) <= ACCEPTABLE_LEARNING_STEP_DIFF && abs(newBias - bias) <= ACCEPTABLE_LEARNING_STEP_DIFF
+	abs(newSlope - slope) <= acceptableLearningStepDiff && abs(newBias - bias) <= acceptableLearningStepDiff
 
 fun getSlopeAfterStep(): Double =
-	slope - (reducedMileageCars.sumByDouble { (estimatePrice(it.mileage) - it.price) * it.mileage } / reducedMileageCars.size) * LEARNING_RATE
+	slope - (reducedMileageCars.sumByDouble { (estimatePrice(it.mileage) - it.price) * it.mileage } / reducedMileageCars.size) * learningRate
 
 fun getBiasAfterStep(): Double =
-	bias - (reducedMileageCars.sumByDouble { estimatePrice(it.mileage) - it.price } / reducedMileageCars.size) * LEARNING_RATE
+	bias - (reducedMileageCars.sumByDouble { estimatePrice(it.mileage) - it.price } / reducedMileageCars.size) * learningRate
 
 fun estimatePrice(mileage: Double) =
 	mileage * slope + bias
+
+fun printResult() {
+	println("Theta0=$bias")
+	println("Theta1=$slope")
+}
 
 fun drawChart() {
 	val chart = createChart()
@@ -134,4 +185,48 @@ fun addLinearRegLineOnChart(chart: XYChart) {
 fun invalidExit(errorMsg: String) {
 	println(errorMsg)
 	exitProcess(1)
+}
+
+class CommandLineArguments(parser: ArgParser) {
+
+	val verbose by parser.flagging(
+		"-v", "--verbose",
+		help = "show verbose output of training progress"
+	)
+
+	val showChart by parser.flagging(
+		"-c", "--chart",
+		help = "show chart of linear regression built"
+	)
+
+	val inputFileName by parser.storing(
+		"-f", "--file",
+		help = "set file as input stream (stdin by default)"
+	).default<String?>(null)
+
+	val learningRage by parser.storing(
+		"-l", "--learning-rate",
+		help = "set learning rate (0.1 by default)"
+	) { toDouble() }
+		.default(DEFAULT_LEARNING_RATE)
+		.addValidator {
+			when {
+				value < 0 -> throw InvalidArgumentException("Learning rate can't be negative")
+				value < 0.0001 -> throw InvalidArgumentException("Learning rate is too small (min is 0.0001)")
+				value > 0.5 -> throw InvalidArgumentException("Learning rate is too large (max is 0.5)")
+			}
+		}
+
+	val acceptableLearningStepDiff by parser.storing(
+		"-d", "--step-diff",
+		help = "set acceptable learning step diff (0.01 by default)"
+	) { toDouble() }
+		.default(DEFAULT_ACCEPTABLE_LEARNING_STEP_DIFF)
+		.addValidator {
+			when {
+				value < 0 -> throw InvalidArgumentException("Step diff can't be negative")
+				value < 0.0001 -> throw InvalidArgumentException("Step diff is too small (min is 0.0001)")
+				value > 1.0 -> throw InvalidArgumentException("Step diff is too large (max is 1)")
+			}
+		}
 }
